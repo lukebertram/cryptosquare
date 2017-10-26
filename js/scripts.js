9aba95b62bf52b7797b25f77f71c6efdcb7e4144
luke bertram and nathan barrett <luke@>
2017-10-26 16:15:55 -0700

    create closetSqaure function, and 2d array

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,49 @@ var closestSquare = function(length){
     counter++;
   }
   // alert("Message length: " + length + "\nSquare: " + square);
-  return square;
+  return counter;
 }
 
 var cryptoSquare = function(textMessage){
   //strip whitespace, punctuation, symbols from user input
-  var cryptoMessage = textMessage.toLowerCase().replace(/\s+/g, '');
+  var message = textMessage.toLowerCase().replace(/\s+/g, '');
   //get the perfect perfect square for given message
-  var perfSquare = closestSquare(cryptoMessage.length);
+  var squareSide = closestSquare(message.length);
+  //create a square 2D array to hold Message
+  var cryptoArray = new Array(squareSide);
+  for (var i = 0; i < cryptoArray.length; i++) {
+    cryptoArray[i] = new Array(squareSide);
+  };
+  var index = 0;
+  //fill up that 2d array with user Message, traverse x axis first
+  for (var y = 0; y < squareSide; y++) { //<= y-axis
+    for (var x = 0; x < squareSide; x++) { //<= x-axis
+      if (index < message.length){
+        cryptoArray[x][y] = message.charAt(index);
+        index++;
+      } else {
+        break;
+      }
+    }
+  };
+  index = 0;
+  var cryptoMessage = "";
+  //read the array back into cryptoMessage, traversing the y axis first
+  for (var x = 0; x < squareSide; x++) { //<= x-axis
+    for (var y = 0; y < squareSide; y++) { //<= y-axis
+      if (index < message.length){
+        if ((index + 1) % 5 === 0){
+          cryptoMessage += " ";
+        }
+         cryptoMessage += cryptoArray[x][y];
+        index++;
+      } else {
+        break;
+      }
+    }
+  };
   return cryptoMessage;
-}
+};
 
 
 
