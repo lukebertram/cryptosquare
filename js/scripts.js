b5472e021ec2b224ad53e2dc336107031e86df3e
luke bertram and nathan barrett <nathan@>
2017-10-26 15:42:05 -0700

    add function to determine appropriately sized square

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 //crypto-comment!
 
+var closestSquare = function(length){
+  var square = 0;
+  var counter = 2;
+  while (square < length) {
+    square = Math.pow(counter,2);
+    counter++;
+  }
+  // alert("Message length: " + length + "\nSquare: " + square);
+  return square;
+}
+
 var cryptoSquare = function(textMessage){
+  //strip whitespace, punctuation, symbols from user input
   var cryptoMessage = textMessage.toLowerCase().replace(/\s+/g, '');
+  //get the perfect perfect square for given message
+  var perfSquare = closestSquare(cryptoMessage.length);
   return cryptoMessage;
 }
 
